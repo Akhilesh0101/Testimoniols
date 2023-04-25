e9ee56ce8cb97275b24501baf0eedd5f4666a697
Akhilesh Tripathi <112152867+Akhilesh0101@users.noreply.github.com>
2023-04-26 02:36:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import reviews from "./data";
+import Testimonials from "./Testimonials";
+
+const App = () => {
+  return(
+    <div className="flex flex-col w-[100vw] h-[100vh] bg-gray-200 justify-center items-center ">
+      <div className="text-center">
+
+        <h1 className="text-4xl font-bold">
+
+          Our Testimonials
+        </h1>
+
+        <div className=" bg-violet-400 h-[4px] w-1/5 mx-auto"></div>
+
+      <Testimonials reviews={reviews}/>
+      </div>
+    </div>
+  );
+};
+
+export default App;
